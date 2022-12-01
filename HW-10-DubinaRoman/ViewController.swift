9b03280e69bdc2ce7004398128915f47edb43a27
RRDubina <dubinaroman.dubina@gmail.com>
2022-12-01 23:49:56 +0500

    Initial Commit

---
+++ HW-10-DubinaRoman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-10-DubinaRoman
+//
+//  Created by admin on 01.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
